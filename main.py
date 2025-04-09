cf1c64b440c26e77df352bcdea74022235e98a48
Kalebdlr <delarosakaleb@gmail.com>
2025-04-09 17:14:25 -0600

    Versión 2: Ver tareas desde archivo

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# main.py
-tarea = input("Escribe una nueva tarea: ")
-with open("tareas.txt", "a") as archivo:
-    archivo.write(tarea + "\n")
-print("Tarea guardada.")
+print("Bienvenido al gestor de tareas\n")
+print("Tus tareas:")
+
+try:
+    with open("tareas.txt", "r") as archivo:
+        tareas = archivo.read()
+        if tareas.strip() == "":
+            print("No tienes tareas pendientes.")
+        else:
+            print(tareas)
+except FileNotFoundError:
+    print("Aún no existe el archivo de tareas.")
